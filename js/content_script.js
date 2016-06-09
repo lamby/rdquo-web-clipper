0fd48c1a246be9841024627687b2e29e3f97d9fd
Chris Lamb <chris@chris-lamb.co.uk>
2016-06-09 21:03:49 +0100

    No need for this..

--- js/content_script.js
+++ js/content_script.js
@@ -1,16 +1,3 @@
-document.addEventListener('mouseup', function (event) {
-  var sel = window.getSelection().toString();
-
-  if (sel.length === 0) {
-    return;
-  }
-
-  chrome.extension.sendRequest({
-    'message': 'setText',
-    'data': sel
-  }, function(response) {});
-});
-
 if (window.location.hostname.indexOf('rdquo.com') === 0) {
   document.documentElement.setAttribute('data-rdquo-web-clipper-installed', true);
 }
